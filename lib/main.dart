38d2e4b90633192c71b1d65c07262392ba360df9
Kelompok7 <123190012@student.upnyk.ac.id>
2022-03-16 11:51:32 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:kuis_tpm/main_screen.dart';
+
+import 'detail_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IF App Store',
+      theme: ThemeData(),
+      home: MainScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
